ec8f121879942ccbb9e1f8b41a8f7adffd385fff
samanthasheadavis <samantha.shea.davis@gmail.com>
2016-10-04 22:09:35 -0400

    built out jquery, added sass and error handler

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
-// Get info from file and when success, trigger
-
+// Get info from file and when success, trigger OrgInfo constructor
 $.ajax({
     type: 'GET',
     url: 'https://api.github.com/users/jisaacks/orgs',
+    error: function(responseObj, textStatus, error) {
+        console.log(responseObj, textStatus, error);
+    },
     success: function(data) {
         return new OrgInfo(data);
     }
+
 });
 
+// OrgInfo constructor
 function OrgInfo(orgObject) {
-    // console.log(orgObject);
     this.info = {
         name: null,
         img: null,
     };
 
+    //  for-in loop that iterates through the seven org objects and stores name and icon image into this.info
     this.getInfo = function() {
         for (var key in orgObject) {
             this.info.name = orgObject[key].login;
-            // this.info.url= orgObject[key].url;
             this.info.img = orgObject[key].avatar_url;
-            console.log(this.info);
             this.createElements();
         }
-
     };
-
+    //createElements method creates an orgInfo container and an <h2> and an <img> element for each org object iterated over.
     this.createElements = function() {
         var container = $('<div>').attr('class', 'orgInfo').appendTo('#container');
         var name = $('<h2>').html(this.info.name).appendTo(container);
@@ -33,8 +34,10 @@ function OrgInfo(orgObject) {
         //$( ".orgInfo" ).after( $( "button" ));
     };
 
+    // the slidetoggle function makes the orginfo elements slide down when the button is clicked, and slide up again when the button is clicked again.
+    $('#btn').on('click', function(event) {
+        $('.orgInfo').slideToggle('slow');
+    });
+
     this.getInfo();
 }
-
-
-// this.createElements();
